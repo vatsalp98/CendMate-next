c35cfcdbabe874fda3fcd71cc44b6160e408a6d3
Vatsaal Parmar <vparmar@sfu.ca>
2024-06-15 16:43:43 +0000

    removed scripts

--- src/app/(landing)/(auth)/kyc/page.tsx
+++ src/app/(landing)/(auth)/kyc/page.tsx
@@ -1,15 +1,9 @@
-import Script from "next/script";
 import KYCCard from "~/app/_components/KYCCard";
 import MaxWidthWrapper from "~/app/_components/MaxWitdhWrapper";
 
 export default function KycPage() {
   return (
     <>
-      <Script src="https://assets.complycube.com/web-sdk/v1/complycube.min.js" />
-      <Script
-        src="https://assets.complycube.com/web-sdk/v1/style.css"
-        rel="stylesheet"
-      />
       <MaxWidthWrapper>
         <div className="flex h-[50vh] w-full flex-col items-center justify-center">
           <KYCCard />
